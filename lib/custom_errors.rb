926b9a5a73dd3cc1fffab20aa027d6fd10428914
fadimata tillman <fadi.tillman@gmail.com>
2020-02-25 02:13:19 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -1,19 +1,3 @@
-class Person
-  attr_accessor :name, :partner
-
-  def initialize(name)
-    @name = name
-  end
-
-  def get_married(person)
-    self.partner = person
-    person.partner = self
-  end
-end
-
-beyonce = Person.new("Beyonce")
-beyonce.get_married("Jay-Z")
-puts beyonce.name
 
 #   class Person
 #   attr_accessor :name, :partner
