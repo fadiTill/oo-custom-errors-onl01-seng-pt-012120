9ca7da71319dcbf33e11d8ff917ec4ba7c9d5a93
fadimata tillman <fadi.tillman@gmail.com>
2020-02-25 02:09:43 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -2,26 +2,26 @@ class Person
 
   class Person
   attr_accessor :name, :partner
- 
+
   def initialize(name)
     @name = name
   end
- 
+
   def get_married(person)
     self.partner = person
     person.partner = self
   end
 end
- 
+
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
 #   attr_accessor :partner, :name
-# 
+#
 #   def initialize(name)
 #     @name = name
 #   end
-# 
+#
 #   def get_married(person)
 #     self.partner = person
 #     if person.class != Person
@@ -30,7 +30,7 @@ puts beyonce.name
 #       person.partner = self
 #     end
 #   end
-# 
+#
 #   class PartnerError < StandardError
 #   end
 # end
